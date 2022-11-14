a563479d348ed93d5dc4d4030fe44f9f2a1d6d60
emrocode <92260849+emrocode@users.noreply.github.com>
2022-11-14 17:56:09 -0300

    Initial commit
    Signed-off-by: emrocode <92260849+emrocode@users.noreply.github.com>

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import router from './v1/routes';
+import { createServer } from 'http';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app
+  .use(express.static(path.join(__dirname, 'assets')))
+  .use(express.urlencoded({ extended: false }))
+  .use(express.json())
+  .use(cors())
+  .use('/v1/myths', router)
+  .set('json spaces', 2)
+  .get('/', (_req, res) =>
+    res.sendFile(path.join(__dirname) + '/assets/index.html')
+  )
+
+const server = createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
+
+export { server };
---
+++ src/v1/routes/index.ts
@@ -0,0 +1,58 @@
+import express from 'express';
+import _ from 'underscore';
+import mythsData from '../../json/data.json';
+import { Response } from '../../types';
+
+const router = express.Router();
+const myths: Array<Response> = mythsData;
+
+router
+  .get('/', (_req, res) => res.status(200).send(myths))
+  .get('/:id', (req, res) => {
+    const id: number = +req.params.id;
+    _.each(myths, (item, i) => {
+      if (item.id == id) return res.status(200).send(myths[i]);
+    });
+
+    if (id <= 0) return res.status(500).redirect('/v1/myths/1');
+    if (id >= 9) return res.status(500).redirect('/v1/myths/8');
+  })
+  .post('/', (req, res) => {
+    const { name, description, image } = req.body;
+    if (name && description && image) {
+      const id = myths.length + 1;
+      const newData = { id, ...req.body };
+      myths.push(newData);
+      return res.send(myths);
+    } else {
+      return res.status(500).send('bad request');
+    }
+  })
+  .put('/:id', (req, res) => {
+    const id: number = +req.params.id;
+    const { name, description, image } = req.body;
+    if (name && description && image) {
+      _.each(myths, item => {
+        if (item.id == id) {
+          item.name = name;
+          item.description = description;
+          item.image = image;
+        }
+      });
+      return res.status(200).send(myths);
+    } else {
+      return res.status(404).send({ error: 'Sorry, cant find that' });
+    }
+  })
+  .delete('/:id', (req, res, _next) => {
+    const id: number = +req.params.id;
+    _.each(myths, (item, i) => {
+      if (item.id == id) {
+        myths.splice(i, 1);
+        return res.status(200).send(`Item ${id} has been deleted`);
+      }
+    });
+    return res.status(200).send(myths);
+  });
+
+export default router;

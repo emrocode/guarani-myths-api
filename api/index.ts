25f0f3ff10eaa8fd63977d9bbe434ce19663e8bc
emrocode <92260849+emrocode@users.noreply.github.com>
2024-04-11 11:30:10 -0400

    Updates

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,26 @@
-export { default as app } from "../backend";
+import express from 'express';
+import cors from 'cors';
+import router from './v1/routes';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app
+  .use(express.urlencoded({ extended: false }))
+  .use(express.json())
+  .use(
+    cors({
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      maxAge: 3600,
+    })
+  )
+  .use('/api/v1/myths', router)
+  .set('json spaces', 2);
+
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
+
+module.exports = app;

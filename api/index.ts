18bb441d70ce476cf34d34ceeba0ba66cff2c7ff
emrocode <92260849+emrocode@users.noreply.github.com>
2024-07-18 15:37:52 -0400

    Minor changes

--- api/index.ts
+++ api/index.ts
@@ -18,12 +18,4 @@ app
   .use('/api/v1/myths', router)
   .set('json spaces', 2);
 
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-
-  app.listen(PORT, () => {
-    console.log(`Server on http://localhost:${PORT}`);
-  });
-}
-
 module.exports = app;

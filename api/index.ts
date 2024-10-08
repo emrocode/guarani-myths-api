e2022d12f44dcd9feb9d7ebef986f98144e3b2cb
emrocode <92260849+emrocode@users.noreply.github.com>
2024-10-08 10:57:33 -0300

    Add v2 routes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,16 @@
 import express, { Application } from 'express';
+import { config } from 'dotenv';
+import connectDB from './db';
 import helmet from 'helmet';
 import cors from 'cors';
 import v1Router from './v1/routes';
+import v2Router from './v2/routes';
+
+// Load env variables
+config();
+
+// Connect to MongoDB
+connectDB();
 
 const app: Application = express();
 
@@ -17,6 +26,7 @@ app
       maxAge: 3600,
     })
   )
-  .use('/api/v1/myths', v1Router);
+  .use('/api/v1/myths', v1Router)
+  .use('/api/v2/myths', v2Router);
 
 export default app;
